9f476116ba3207ae55a0b888f3735bb6d98f7011
larysa-tolkachova <seagull2610@gmail.com>
2025-01-14 11:18:45 +0000

    Deploying to gh-pages from @ larysa-tolkachova/goit-js-hw-11@dffeec323d372cedafd9a3849a9aebdd6f435086 🚀

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 /* empty css                      */import{i as a,S as m}from"./assets/vendor-5ObWk2rO.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const i of o.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&l(i)}).observe(document,{childList:!0,subtree:!0});function t(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function l(e){if(e.ep)return;e.ep=!0;const o=t(e);fetch(e.href,o)}})();const u="https://pixabay.com/api/",h="47526493-0201b9f5d9dd403f56de0598a";function d(s){const r=new URLSearchParams({key:h,q:s,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`${u}?${r}`).then(t=>{if(!t.ok)throw new Error(t.statusText);return t.json()})}function g(s){return s.map(({webformatURL:r,largeImageURL:t,tags:l,likes:e,views:o,comments:i,downloads:f})=>`<li class="gallery-item>
             <a class="gallery-link" href="${t}">
-                <img class="gallery-image" src="${r}" alt="${l}" width="360" height="150"/>
+                <img class="gallery-image" src="${r}" alt="${l}" width="360"/>
             </a>
                 <ul class="inform">
                     <li class="inform-link">
                         <h2 class="inform-title">Likes:</h2>
-                        <p class="inform-dan>${e}</p>
+                        <p class="inform-dan">${e}</p>
                     </li>
                     <li class="inform-link">
                         <h2 class="inform-title">Views:</h2>
-                        <p class="inform-dan>${o}</p>
+                        <p class="inform-dan">${o}</p>
                     </li>
                     <li class="inform-link">
                         <h2 class="inform-title">Comments:</h2>
-                        <p class="inform-dan>${i}</p>
+                        <p class="inform-dan">${i}</p>
                     </li>
                     <li class="inform-link">
                         <h2 class="inform-title">Downloads:</h2>
-                        <p class="inform-dan>${f}</p>
+                        <p class="inform-dan">${f}</p>
                     </li>
                 </ul>
             
